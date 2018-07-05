219118264dc79f418312d68f074ce3be85d39dc0
Karen Guo <karenguo@live.com>
2018-07-04 17:47:54 -0700

    Add remove and heapify functions

--- min-heap-map.js
+++ min-heap-map.js
@@ -23,6 +23,59 @@ module.exports = class MinHeapMap {
 
   // O(log(n))
   removeMin() {
+    if (this.heap.length < 2) {
+      return this.heap.pop();
+    }
+    // Min is the first item
+    let min = this.heap[0];
+    let lastItem = this.heap[this.heap.length-1];
 
+    // Swap the root with the last leaf
+    this.heap[this.heap.length-1] = min;
+    this.heap[0] = lastItem;
+
+    // Remove the last item which is the min now
+    this.heap.pop();
+
+    this.heapify();
+
+    return min;
+  }
+
+  heapify() {
+    let index = 0;
+    let leftChildIndex = this.getLeftChildIndex(index);
+    let rightChildIndex = this.getRightChildIndex(index);
+
+    // What if right or left child doesn't exist?
+    while (
+      this.heap[index].distance < this.heap[leftChildIndex].distance ||
+      this.heap[index].distance < this.heap[rightChildIndex].distance
+    ) {
+      let indexValue = this.heap[index];
+      // left child is larger
+      if (this.heap[leftChildIndex].distance > this.heap[rightChildIndex].distance) {
+        // Swap index with left child
+        this.heap[index] = this.heap[leftChildIndex];
+        this.heap[leftChildIndex] = indexValue;
+        index = leftChildIndex;
+        leftChildIndex = this.getLeftChildIndex(index);
+        rightChildIndex = this.getRightChildIndex(index);
+      } else { // right child is larger
+        this.heap[index] = this.heap[rightChildIndex];
+        this.heap[rightChildIndex] = indexValue;
+        index = rightChildIndex;
+        leftChildIndex = this.getLeftChildIndex(index);
+        rightChildIndex = this.getRightChildIndex(index);
+      }
+    }
+  }
+
+  getLeftChildIndex(index) {
+    return index * 2 + 1;
+  }
+
+  getRightChildIndex(index) {
+    return index * 2 + 2;
   }
 }
