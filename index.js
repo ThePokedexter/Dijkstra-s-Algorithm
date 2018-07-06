02610a7412df9da3a831a11a2c6c06b5c354a3b8
Karen Guo <karenguo@live.com>
2018-07-06 14:54:38 -0700

    Get data from txt files

--- index.js
+++ index.js
@@ -1,26 +1,47 @@
-var args = process.argv.slice(2);
+const fs = require('fs');
+const Graph = require('./graph');
+const g = new Graph();
 
-const Graph = require('./graph.js');
+const graphFile = process.argv[2];
+const inputFile = process.argv[3];
 
-var g = new Graph();
-g.addVertex('AB5');
-g.addVertex('BC4');
-g.addVertex('CD8');
-g.addVertex('DC8');
-g.addVertex('DE6');
-g.addVertex('AD5');
-g.addVertex('CE2');
-g.addVertex('EB3');
-g.addVertex('AE7');
+function main() {
+  readGraphAndInput();
+}
 
-console.log(g.vertices);
-console.log('1. The distance of the route A-B-C: ' + g.lengthOfPath('ABC'));
-console.log('2. The distance of the route A-D: ' + g.lengthOfPath('AD'));
-console.log('3. The distance of the route A-D-C: ' + g.lengthOfPath('ADC'));
-console.log('4. The distance of the route A-E-B-C-D: ' + g.lengthOfPath('AEBCD'));
-console.log('5. The distance of the route A-E-D: ' + g.lengthOfPath('AED'));
-console.log('6. The number of trips starting at C and ending at C with a maximum of 3 stops: ' + g.numPathsMaxStops('C', 'C', 3));
-console.log('7. The number of trips starting at A and ending at C with exactly 4 stops: ' + g.numPaths('A', 'C', 4));
-console.log('8. The length of the shortest route (in terms of distance to travel) from A to C: ' + g.shortestPath('A', 'C'));
-console.log('9. The length of the shortest route (in terms of distance to travel) from B to B: ' + g.shortestPath('B', 'B'));
-console.log('10. The number of different routes from C to C with a distance of less than 30: ' + g.numPathsMaxDistance('C', 'C', 30));
+function readGraphAndInput(g) {
+  fs.readFile(graphFile, 'utf-8', readFileCallback)
+};
+
+function readFileCallback(error, data) {
+  if (error) throw error;
+  const graphData = data.split(', ');
+
+  for (let connection of graphData) {
+    g.addVertex(connection);
+  }
+
+  readInputFile();
+}
+
+function readInputFile() {
+  fs.readFile(inputFile, 'utf-8', readInputFileCallback);
+}
+
+function readInputFileCallback(error, data) {
+  if (error) throw error;
+
+  console.log(g.vertices);
+  console.log('1. The distance of the route A-B-C: ' + g.lengthOfPath('ABC'));
+  console.log('2. The distance of the route A-D: ' + g.lengthOfPath('AD'));
+  console.log('3. The distance of the route A-D-C: ' + g.lengthOfPath('ADC'));
+  console.log('4. The distance of the route A-E-B-C-D: ' + g.lengthOfPath('AEBCD'));
+  console.log('5. The distance of the route A-E-D: ' + g.lengthOfPath('AED'));
+  console.log('6. The number of trips starting at C and ending at C with a maximum of 3 stops: ' + g.numPathsMaxStops('C', 'C', 3));
+  console.log('7. The number of trips starting at A and ending at C with exactly 4 stops: ' + g.numPaths('A', 'C', 4));
+  console.log('8. The length of the shortest route (in terms of distance to travel) from A to C: ' + g.shortestPath('A', 'C'));
+  console.log('9. The length of the shortest route (in terms of distance to travel) from B to B: ' + g.shortestPath('B', 'B'));
+  console.log('10. The number of different routes from C to C with a distance of less than 30: ' + g.numPathsMaxDistance('C', 'C', 30));
+}
+
+main();
